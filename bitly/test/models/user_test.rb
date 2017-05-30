6fb0c08eecdcd4515f6ede8b1bb1c6814b21f8b9
Erik Thiem <ethiem@covermymeds.com>
2017-05-30 13:20:03 -0400

    Added user tests

--- bitly/test/models/user_test.rb
+++ bitly/test/models/user_test.rb
@@ -4,4 +4,61 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should create a new User" do
+      user = User.new(
+        email: "test@email.com",
+        password: "test123",
+        password_confirmation: "test123"
+      )
+
+      assert user.save
+  end
+
+  test "should not create a new User if the password is empty" do
+      user = User.new(
+        email: "test@email.com",
+        password: "",
+        password_confirmation: ""
+      )
+
+      assert_not user.save
+  end
+
+  test "should not create a new User if the username is empty" do
+      user = User.new(
+        email: "",
+        password: "test123",
+        password_confirmation: "test123"
+      )
+
+      assert_not user.save
+  end
+
+  test "should not create a new User if the password does not match the confirmation" do
+      user = User.new(
+        email: "test@email.com",
+        password: "test123",
+        password_confirmation: "test1234"
+      )
+
+      assert_not user.save
+  end
+
+  test "should not allow a duplicate email" do
+      user = User.create(
+        email: "test@email.com",
+        password: "test123",
+        password_confirmation: "test1234"
+      )
+
+      user_duplicate = User.new(
+        email: "test@email.com",
+        password: "test123",
+        password_confirmation: "test1234"
+      )
+
+      assert_not user_duplicate.save
+  end
+
 end
