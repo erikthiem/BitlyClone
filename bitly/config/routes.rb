da1fe24786daad1645ff2295a26f32a0e9f9240e
Erik Thiem <ethiem@covermymeds.com>
2017-05-30 13:24:19 -0400

    Added welcome page for unauthenticated visitors

--- bitly/config/routes.rb
+++ bitly/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   resources :urls
-  root 'urls#index'
+  root 'welcome#index'
 
   get 'go/:shortcode', to: 'urls#go'
 end
